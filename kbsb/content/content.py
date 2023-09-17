06fc9eeec85b6f6277164a6d677b9876b14f1c70
Ruben Decrop <ruben@decrop.net>
2023-09-17 21:30:45 +0200

    move to standard lru cache

--- kbsb/content/content.py
+++ kbsb/content/content.py
@@ -8,7 +8,7 @@ import frontmatter
 from google.cloud import storage
 from google.cloud.storage import Blob
 from google.api_core import exceptions
-from expiring_lru_cache import lru_cache, HOURS
+from functools import lru_cache
 
 from reddevil.core import (
     RdInternalServerError,
@@ -47,7 +47,7 @@ def get_articles() -> List[Article]:
     return articles
 
 
-@lru_cache(expires_after=1 * HOURS, maxsize=30)
+@lru_cache(maxsize=30)
 def get_article(slug: str) -> Article:
     """
     read the file from the filestore
